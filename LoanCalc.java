a671d29df84b614ee82ebbbdf6d4480009679a53
udi hochberg <udi.hochberg@post.runi.ac.il>
2024-11-26 19:31:02 +0200

    run 1

--- LoanCalc.java
+++ LoanCalc.java
@@ -27,19 +27,24 @@ public class LoanCalc {
 
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
-	//private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-	//	return 0;
-	//}
+	private static double endBalance(double num0, double num2, int num4, double num5) {	
+		double num7 = num0;
+		double num9 = num2 / 100.0;
+		for (int i = 0; i < num4; i++) { num7 = (num7 - 5) * (num9 + 1.0); }
+		return num7;
+		
+	}
 	
-	// Uses sequential search to compute an approximation of the periodical payment
-	// that will bring the ending balance of a loan close to 0.
-	// Given: the sum of the loan, the periodical interest rate (as a percentage),
-	// the number of periods (n), and epsilon, the approximation's accuracy
-	// Side effect: modifies the class variable iterationCounter.
+	
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		double num1 = loan / (double) n;
+		double num9 = endBalance(loan, rate, n, num1);
+		for (int i = 0; num9 > 0; num9 = endBalance(loan, rate, n, num1))
+		{
+			i++;
+			num1 += epsilon;
+		}
+		return num1;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -48,7 +53,21 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+		int counter = 0;
+		double num7 = rate / 100.0;
+		double num9 = 0.0;
+		double num11 = loan;
+		double num13 = 0.0;
+		double num15 = 0.0;
+
+		while ((num11 - num9) > epsilon)
+		{
+			num13 = (num9 + num11) / 2.0;
+			num15 = endBalance(loan, rate, n, num13);
+			counter++;
+			if (num15 > 0) { num9 = num13; }
+			else { num11 = num13;}
+		}
+		return num13;
     }
 }
